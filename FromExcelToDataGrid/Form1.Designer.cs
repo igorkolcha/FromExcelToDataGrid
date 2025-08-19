6439e64c16c1af04234b47b248657f1e07fa1c54
igor.kolcha <igor.kolcha@list.ru>
2025-08-19 16:27:10 +0300

    Добавьте файлы проекта.

---
+++ FromExcelToDataGrid/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace FromExcelToDataGrid
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGrid = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)DataGrid).BeginInit();
+            SuspendLayout();
+            // 
+            // DataGrid
+            // 
+            DataGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            DataGrid.Location = new Point(12, 12);
+            DataGrid.Name = "DataGrid";
+            DataGrid.Size = new Size(776, 224);
+            DataGrid.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(DataGrid);
+            Name = "Form1";
+            Text = "FromExcelToDataGrid";
+            ((System.ComponentModel.ISupportInitialize)DataGrid).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView DataGrid;
+    }
+}
---
+++ FromExcelToDataGrid/Form1.cs
@@ -0,0 +1,10 @@
+namespace FromExcelToDataGrid
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
