5234a6ff5359cb31d4f0c761bd1af4fcb3a828f5
igor.kolcha <igor.kolcha@list.ru>
2025-09-21 17:45:28 +0300

    added a button Select Excel Download DataGrid, methods DataTable LoadExcelToDataTable, SelectExcelDownloadDataGrid_Click

--- FromExcelToDataGrid/Form1.Designer.cs
+++ FromExcelToDataGrid/Form1.Designer.cs
@@ -29,23 +29,36 @@
         private void InitializeComponent()
         {
             DataGrid = new DataGridView();
+            SelectExcelDownloadDataGrid = new Button();
             ((System.ComponentModel.ISupportInitialize)DataGrid).BeginInit();
             SuspendLayout();
             // 
             // DataGrid
             // 
             DataGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            DataGrid.Location = new Point(12, 12);
+            DataGrid.Location = new Point(14, 14);
             DataGrid.Name = "DataGrid";
-            DataGrid.Size = new Size(776, 224);
+            DataGrid.Size = new Size(887, 254);
             DataGrid.TabIndex = 0;
+            // 
+            // SelectExcelDownloadDataGrid
+            // 
+            SelectExcelDownloadDataGrid.Location = new Point(310, 341);
+            SelectExcelDownloadDataGrid.Name = "SelectExcelDownloadDataGrid";
+            SelectExcelDownloadDataGrid.Size = new Size(295, 68);
+            SelectExcelDownloadDataGrid.TabIndex = 1;
+            SelectExcelDownloadDataGrid.Text = "Select Excel Download DataGrid";
+            SelectExcelDownloadDataGrid.UseVisualStyleBackColor = true;
+            SelectExcelDownloadDataGrid.Click += SelectExcelDownloadDataGrid_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 510);
+            Controls.Add(SelectExcelDownloadDataGrid);
             Controls.Add(DataGrid);
+            Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 204);
             Name = "Form1";
             Text = "FromExcelToDataGrid";
             ((System.ComponentModel.ISupportInitialize)DataGrid).EndInit();
@@ -55,5 +68,6 @@
         #endregion
 
         private DataGridView DataGrid;
+        private Button SelectExcelDownloadDataGrid;
     }
 }
--- FromExcelToDataGrid/Form1.cs
+++ FromExcelToDataGrid/Form1.cs
@@ -1,3 +1,7 @@
+﻿using ClosedXML;
+using ClosedXML.Excel;
+using System.Data;
+
 namespace FromExcelToDataGrid
 {
     public partial class Form1 : Form
@@ -8,7 +12,58 @@ namespace FromExcelToDataGrid
 
             //this.WindowState = FormWindowState.Maximized;
             this.DataGrid.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            this.DataGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            this.DataGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;          
+        }
+
+        private void SelectExcelDownloadDataGrid_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Excel Files|*.xlsx;*.xls";
+                ofd.Title = "Select Excel file";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = ofd.FileName;
+                    DataTable dt = LoadExcelToDataTable(filePath);
+                    DataGrid.DataSource = dt;
+                    //DataGrid.Columns[0].Visible = false;
+                }
+            }
+        }
+
+        private DataTable LoadExcelToDataTable(string filePath)
+        {
+            DataTable dt = new DataTable();
+
+            using (var workbook = new XLWorkbook(filePath))
+            {
+                var worksheet = workbook.Worksheet(1); // first sheet
+                bool firstRow = true;
+
+                foreach (var row in worksheet.RowsUsed())
+                {
+                    if (firstRow)
+                    {
+                        // headlines
+                        foreach (var cell in row.CellsUsed())
+                            dt.Columns.Add(cell.Value.ToString());
+                        firstRow = false;
+                    }
+                    else
+                    {
+                        dt.Rows.Add();
+                        int i = 0;
+                        foreach (var cell in row.Cells(1, dt.Columns.Count))
+                        {
+                            dt.Rows[dt.Rows.Count - 1][i] = cell.Value.ToString();
+                            i++;
+                        }
+                    }
+                }
+            }
+
+            return dt;
         }
     }
 }
